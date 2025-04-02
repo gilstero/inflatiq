2a5e5a8674ee2871e67906b2960ed36c41581335
Olin Gilster <olin.gilster@icloud.com>
2025-04-01 20:59:18 -0400

    added landing page for trial

--- inflatiq/src/app/login/page.tsx
+++ inflatiq/src/app/login/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Login() {
@@ -15,9 +14,46 @@ export default function Login() {
                 alt="Company Logo"
                 className="logo mb-4"
             />
-            <div className="loginbox items-center shadow-neon-blue">
-            </div>
+            <title>Welcome.</title>
+            <div className="loginbox flex flex-col items-center justify-center shadow-neon-blue p-6 rounded-lg w-80">
+                <p className="text-3xl mb-10">
+                    Welcome
+                </p>
+                <label htmlFor="email" className="sr-only">
+                </label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Enter Email"
+                    required
+                    className="input-field mb-3 p-2 border border-gray-300 rounded"
+                />
 
+                <label htmlFor="psw" className="self-start mb-1 font-bold">
+                </label>
+                <input
+                    type="password"
+                    id="psw"
+                    name="psw"
+                    placeholder="Enter Password"
+                    required
+                    className="input-field mb-3 p-2 border border-gray-300 rounded"
+                />
+                <button className="w-55 py-4 px-6 mt-10 rounded-full bg-neon-blue text-white font-medium hover:bg-neon-blue-dark transition duration-300 shadow-neon-blue hover:shadow-neon-blue"
+                onClick={() => router.push('/signup')}>
+                Login
+                </button>
+                <a className="mt-4" onClick={() => router.push('/signup')}>
+                    Make an account?
+                </a>
+            </div>
+            <div className="justify-center">
+            <a href="/" className="mt-6 underline mr-2" >home</a>
+            <a href="/" className="mt-6 underline ml-2 mr-2">API</a>
+            <a href="/" className="mt-6 underline ml-2 mr-2">privacy policy</a>
+            <a href="/" className="mt-6 underline ml-2 ">our goals</a>
+            </div>
         </div>
     );
 }
--- inflatiq/src/app/trial/page.tsx
+++ inflatiq/src/app/trial/page.tsx
@@ -1,3 +1,26 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+
+export default function Trial() {
+
+    
+
+
+    return (
+        <div className="flex flex-col justify-center items-center font-sans">
+            <div className="flex justify-end items-center space-x-4 w-full navbar">
+                <button className="float-right w-55 py-4 px-6 mr-10 rounded-full bg-neon-blue text-white font-medium hover:bg-neon-blue-dark transition duration-300">
+                Profile
+                </button> 
+                <button className="float-right w-55 py-4 px-6 mr-10 rounded-full bg-neon-blue text-white font-medium hover:bg-neon-blue-dark transition duration-300">
+                Sign Up
+                </button>
+                <button> </button>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
