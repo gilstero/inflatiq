dd514ea65590fc05681557f7bf551d88d2eaee57
Olin Gilster <olin.gilster@icloud.com>
2025-04-06 16:34:35 -0400

    added the ability to login in the backend

--- server/members/views.py
+++ server/members/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from .serializer import *
 
 
 class userview(APIView):
@@ -26,7 +28,14 @@ class register(APIView):
     permission_classes = (AllowAny,)
 
     def post(self, request):
-        pass
+        serializer = RegisterSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=201)
+        
+        return Response(serializer.errors, status=400)
+
 
 
 
