049c5b878d1ed7cde3fb6a5d5fc9ad3ff8085ac0
Olin Gilster <olin.gilster@icloud.com>
2025-04-06 13:21:43 -0400

    added first url for api calls to backend

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from members.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/private/userpage", userview.as_view(), name="userviewer"),
+    
 ]
